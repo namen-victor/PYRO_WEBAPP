a8e7e334400b969999ed2a949a2658df953bf8b2
pyrosolutionsinc <pyrosolutionsinc@gmail.com>
2025-10-26 18:28:35 -0400

    fix: update middleware matcher to properly exclude static files

--- src/middleware.ts
+++ src/middleware.ts
@@ -85,9 +85,9 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
-     * - public folder files
+     * - static files (.*\\..*)
      */
-    '/((?!api|_next/static|_next/image|favicon.ico|.*\\..*|public).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|.*\\..*).*)',
   ],
 };
 
